3b7d7a1d8e4da776575d53cd4844097eaa9a9fda
ThanyaphornSaetao347 <142294145+ThanyaphornSaetao347@users.noreply.github.com>
2025-08-07 15:15:47 +0700

    แก้ ticket.module file

--- ticket-microservice/src/ticket/ticket.module.ts
+++ ticket-microservice/src/ticket/ticket.module.ts
@@ -6,14 +6,12 @@ import { Ticket } from './entities/ticket.entity';
 import { TicketAttachment } from '../ticket_attachment/entities/ticket_attachment.entity';
 import { AttachmentService } from '../ticket_attachment/ticket_attachment.service';
 import { TicketAttachmentController } from '../ticket_attachment/ticket_attachment.controller';
-import { TicketCategory } from '../ticket_categories/entities/ticket_category.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Ticket,
       TicketAttachment,
-      TicketCategory,
     ]),
   ],
   controllers: [TicketController, TicketAttachmentController],
